c731d41f9a5233f5698311941ac8de1212872c49
Lynx <141365347+iLynxcat@users.noreply.github.com>
2024-11-10 02:36:00 -0800

    annotate IBM Plex Sans weight imports

--- packages/ui/style/index.js
+++ packages/ui/style/index.js
@@ -1,8 +1,8 @@
 import './style.scss';
-import '@fontsource/ibm-plex-sans/100.css';
-import '@fontsource/ibm-plex-sans/200.css';
-import '@fontsource/ibm-plex-sans/300.css';
-import '@fontsource/ibm-plex-sans/400.css';
-import '@fontsource/ibm-plex-sans/500.css';
-import '@fontsource/ibm-plex-sans/600.css';
-import '@fontsource/ibm-plex-sans/700.css';
+import '@fontsource/ibm-plex-sans/100.css'; // Thin
+import '@fontsource/ibm-plex-sans/200.css'; // Extralight
+import '@fontsource/ibm-plex-sans/300.css'; // Light
+import '@fontsource/ibm-plex-sans/400.css'; // Regular
+import '@fontsource/ibm-plex-sans/500.css'; // Medium
+import '@fontsource/ibm-plex-sans/600.css'; // Semibold
+import '@fontsource/ibm-plex-sans/700.css'; // Bold
