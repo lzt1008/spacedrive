124db3442f9b2b7f457227a60517eabef524009f
Lynx <141365347+iLynxcat@users.noreply.github.com>
2024-11-10 06:33:07 -0800

    update `font-tiny` to even 10px and annotate all equivalent pixel values

--- packages/ui/style/tailwind.js
+++ packages/ui/style/tailwind.js
@@ -27,18 +27,18 @@ module.exports = function (app, options) {
 				plex: ['IBM Plex Sans', ...defaultTheme.fontFamily.sans]
 			},
 			fontSize: {
-				'tiny': '.65rem',
-				'xs': '.75rem',
-				'sm': '.80rem',
-				'base': '1rem',
-				'lg': '1.125rem',
-				'xl': '1.25rem',
-				'2xl': '1.5rem',
-				'3xl': '1.875rem',
-				'4xl': '2.25rem',
-				'5xl': '3rem',
-				'6xl': '4rem',
-				'7xl': '5rem'
+				'tiny': '.625rem', // 10px
+				'xs': '.75rem', // 12px
+				'sm': '.875rem', // 14px
+				'base': '1rem', // 16px
+				'lg': '1.125rem', // 18px
+				'xl': '1.25rem', // 20px
+				'2xl': '1.5rem', // 24px
+				'3xl': '1.875rem', // 30px
+				'4xl': '2.25rem', // 36px
+				'5xl': '3rem', // 48px
+				'6xl': '4rem', // 64px
+				'7xl': '5rem' // 80px
 			},
 			extend: {
 				colors: {
